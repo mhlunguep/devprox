a3c24df67388783935d89855ecb581735756cd68
Phumlani Mhlungu <74601957+mhlunguep@users.noreply.github.com>
2022-06-30 03:07:27 +0200

    Delete app directory

--- app/config/_env.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//define the base path
-define('BASE_PATH',  realpath(__DIR__.'/../../'));
-
-require_once __DIR__.'/../../vendor/autoload.php'; 
-
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(BASE_PATH);
-$dotenv->Load();
